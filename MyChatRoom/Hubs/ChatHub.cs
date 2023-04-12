fc2f15f0011de7fc830b5abef6857cea1b23eb51
Najeebullah <Najeebullah@DESKTOP-K5N4NQ2>
2023-04-12 20:10:02 +0500

    initial

---
+++ MyChatRoom/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using MyChatRoom.Services;
+using System;
+using System.Threading.Tasks;
+
+namespace MyChatRoom.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public readonly ChatService _chatService;
+        public ChatHub(ChatService chatService)
+        {
+            _chatService= chatService;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "MyChatRoom");
+            await Clients.Caller.SendAsync("UserConnected");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Groups.RemoveFromGroupAsync(Context.ConnectionId, "MyChatRomm");
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
